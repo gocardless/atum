1cac9726ebddeef7923b495bf0d9b17fc5b0e27f
Pete Hamilton <peterejhamilton@gmail.com>
2014-10-09 17:40:37 +0100

    Nuke another method_missing in favour of define_singleton_method <3

--- lib/atum/core/resource.rb
+++ lib/atum/core/resource.rb
@@ -6,27 +6,9 @@ module Atum
       #
       # @param links [Hash<String,Link>] A hash that maps method names to links.
       def initialize(links)
-        @links = links
-      end
-
-      def respond_to?(name)
-        @links.key?(name.to_s)
-      end
-
-      private
-
-      # Find a link and run it.
-      #
-      # @param name [String] The name of the method to invoke.
-      # @param parameters [Array] The arguments to pass to the method.  This
-      #   should always be a `Hash` mapping parameter names to values.
-      # @raise [NoMethodError] Raised if the name doesn't match a known link.
-      # @return [String,Array,Hash] The response received from the server.  JSON
-      #   responses are automatically decoded into Ruby objects.
-      def method_missing(name, *parameters)
-        super unless respond_to?(name)
-
-        @links[name.to_s].run(*parameters)
+        links.each do |name, link|
+          define_singleton_method(name) { |*args| link.run(*args) }
+        end
       end
     end
   end
